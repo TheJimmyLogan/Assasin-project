b266d25a68f5d1e38f7ba7682f574a52e1e5d28c
Эрик Э <rick-jimmy@mail.ru>
2022-10-29 16:30:58 +0300

    fixed add

--- js/common.js
+++ js/common.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	const showSlider = new Swiper('.showcase-carousel', {
 		loop: true,
 		slidesPerView: 3,
-		speed: 1800,
+		speed: 800,
 		centeredSlides: true,
 		navigation: {
 			nextEl: '.showcase-navigation__next',
